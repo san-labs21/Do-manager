f1cc98e4c86a70a46b3c118add23680c269aadde
san-labs21 <01sandhy@gmail.com>
2025-06-03 12:01:33 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
- wget -q https://raw.githubusercontent.com/san-labs21/Do-manager/main/main.sh.x
- chmod +x main.sh.x
- ./main.sh.x
+wget -q https://raw.githubusercontent.com/san-labs21/Do-manager/main/main.sh.x
+
+chmod +x main.sh.x
+
+./main.sh.x
