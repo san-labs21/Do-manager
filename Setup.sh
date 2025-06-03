cceec3f1848797d8328bde974aa32b09abb8297a
san-labs21 <01sandhy@gmail.com>
2025-06-03 11:53:26 +0800

    Update and rename main.sh to Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,3 @@
+ wget -q https://raw.githubusercontent.com/san-labs21/Do-manager/main/main.sh.x
+ chmod +x main.sh.x
+ ./main.sh.x
--- main.sh
+++
@@ -1,3 +0,0 @@
-wget -q https://raw.githubusercontent.com/Sandhj/tools/main/setup.sh
-chmod +x setup.sh
-bash setup.sh
